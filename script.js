d9e67db07bd3e5a010e4d44a27b4fef81c332789
Darla <dpoling1@unm.edu>
2026-08-05 13:26:55 -0600

    Update fetch URL for IP submission to Google Sheet

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ gtag('config', 'G-RL9MNSLQTW');
 
 // Send IP to Google Sheet
 function getIP(json) {
-    fetch('https://script.google.com/macros/s/AKfycbx5vCd1Y7ioNUC57Fi70lvi5eipSkPrwgdMytxCNmWmC4M0cpKU6GXgDYAViY1tCgt9/exec', {
+    fetch('https://script.google.com/macros/s/AKfycbzVlTJVRxoDCTaekd4D2q7IoJd8lQJqgCdZJCyPR3nHfW51DXnHfcOasIWnBrGYDBK0/exec', {
         method: 'POST',
         mode: 'no-cors', // required for Apps Script
         body: JSON.stringify({ ip: json.ip })
