cfcc594ff5e78e6de77dbd604ee7cb47da5e4b8c
Darla <dpoling1@unm.edu>
2026-08-05 12:57:41 -0600

    Add script.js for Google Analytics and IP retrieval

---
+++ script.js
@@ -0,0 +1,12 @@
+
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+  
+    gtag('config', 'G-RL9MNSLQTW');
+
+
+type="application/javascript">
+    function getIP(json) {
+      document.write("My public IP address is: ", json.ip);
+    }
